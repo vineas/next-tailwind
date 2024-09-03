578ee823a87712ec9b586844c885c6be7c33ac2d
vineas <alvienasyandika@gmail.com>
2024-09-03 17:40:19 +0700

    first commit

---
+++ src/components/Hero/index.jsx
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <div className='bg-gray-900'>
+            <div className='container mx-auto text-center text-white h-screen flex items-center'>
+                <div className='mx-auto w-2/4'>
+                    <h3 className='text-5xl font-bold text-center mb-5'>Bring Your Business to The Next Level</h3>
+                    <p className='text-gray-500 mb-5'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet enim nam ex mollitia nulla a ab quos voluptates nemo eum?</p>
+                    <Link href={"/"} >
+                        <h5 className='bg-red-500 text-white py-2 rounded-xl hover:bg-red-800'>See All Portofolio</h5>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import Hero from "@/components/Hero";
+import Navbar from "@/components/Navbar";
+import Portofolio from "@/components/Portofolio";
+import Services from "@/components/Services";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <>
+      <Navbar/>
+      <Hero/>
+      <Services/>
+      <Portofolio/>
+    </>
+  );
+}

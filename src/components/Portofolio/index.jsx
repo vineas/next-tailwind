7199c66fcf1f86ed0ecbe3271cfe8c83b3552098
vineas <alvienasyandika@gmail.com>
2024-09-03 18:04:50 +0700

    add about

--- src/components/Portofolio/index.jsx
+++ src/components/Portofolio/index.jsx
@@ -8,41 +8,45 @@ const Portofolio = () => {
             <p className='text-gray-500 text-center'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga, veritatis.</p>
             <div className='grid grid-cols-4 container mx-auto gap-8'>
                 {/* portofolio card 1 */}
-                <div className='shadow-xl p-5'>
+                <div className='shadow-xl'>
                     <Link href={"/"}>
-                        <img src="https://placehold.co/600x400" className='w-full'/>
+                        <img src="https://placehold.co/600x400" className='w-full' />
+
                         <div className='py-3 px-5'>
                             <h4 className='text-center font-bold'>Website 1</h4>
                         </div>
                     </Link>
                 </div>
                 {/* portofolio card 2 */}
-                <div className='shadow-xl p-5'>
+                <div className='shadow-xl'>
                     <Link href={"/"}>
-                        <img src="https://placehold.co/600x400" className='w-full'/>
+                        <img src="https://placehold.co/600x400" className='w-full' />
+
                         <div className='py-3 px-5'>
-                            <h4 className='text-center font-bold'>Website 1</h4>
+                            <h4 className='text-center font-bold'>Website 2</h4>
                         </div>
                     </Link>
                 </div>
                 {/* portofolio card 3 */}
-                <div className='shadow-xl p-5'>
+                <div className='shadow-xl'>
                     <Link href={"/"}>
-                        <img src="https://placehold.co/600x400" className='w-full'/>
+                        <img src="https://placehold.co/600x400" className='w-full' />
+
                         <div className='py-3 px-5'>
-                            <h4 className='text-center font-bold'>Website 1</h4>
+                            <h4 className='text-center font-bold'>Website 3</h4>
                         </div>
                     </Link>
                 </div>
                 {/* portofolio card 4 */}
-                <div className='shadow-xl p-5'>
+                <div className='shadow-xl'>
                     <Link href={"/"}>
-                        <img src="https://placehold.co/600x400" className='w-full'/>
+                        <img src="https://placehold.co/600x400" className='w-full' />
+
                         <div className='py-3 px-5'>
-                            <h4 className='text-center font-bold'>Website 1</h4>
+                            <h4 className='text-center font-bold'>Website 4</h4>
                         </div>
                     </Link>
-                </div>                                                                
+                </div>
             </div>
         </div>
     )
